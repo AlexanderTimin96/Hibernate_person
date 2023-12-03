8bf1b7778de8cc15583a5906e6a375e41d004035
Cube_Rubick666 <Timin.1996@list.ru>
2023-12-03 19:29:31 +0300

    Spring security (#2)

--- src/main/java/ru/netology/hibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/hibernate/repository/PersonRepository.java
@@ -1,15 +1,19 @@
 package ru.netology.hibernate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.netology.hibernate.entity.Person;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface PersonRepository  extends JpaRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    @Query("select p from Person p where p.cityOfLiving = :city")
     List<Person> findByCityOfLiving(String city);
 
+    @Query("select p from Person p where p.age < :age order by p.age asc")
     List<Person> findByAgeLessThanOrderByAgeAsc(Integer age);
 
+    @Query("select p from Person p where p.name = :name and p.surname = :surname")
     Optional<Person> findByNameAndSurname(String name, String surname);
 }
